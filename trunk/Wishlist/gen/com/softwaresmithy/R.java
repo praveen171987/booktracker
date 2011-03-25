aa97279d7ecf2cb7350cbbc3b6d846e009dded50
levan.matthew <levan.matthew@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-03-25 03:43:02 +0000

    working menu and text input box

--- trunk/Wishlist/gen/com/softwaresmithy/R.java
+++ trunk/Wishlist/gen/com/softwaresmithy/R.java
@@ -14,13 +14,21 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int text1=0x7f050000;
+        public static final int add_barcode=0x7f060003;
+        public static final int add_text=0x7f060002;
+        public static final int input_isbn=0x7f060001;
+        public static final int text1=0x7f060000;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int main=0x7f030001;
     }
+    public static final class menu {
+        public static final int main=0x7f050000;
+    }
     public static final class string {
+        public static final int add_barcode=0x7f040003;
+        public static final int add_text=0x7f040002;
         public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
--- trunk/Wishlist/src/com/softwaresmithy/Wishlist.java
+++ trunk/Wishlist/src/com/softwaresmithy/Wishlist.java
@@ -3,12 +3,20 @@ package com.softwaresmithy;
 import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.EditText;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class Wishlist extends ListActivity {
 	
 	private int mNoteNumber = 1;
 	private WishlistDbAdapter mDbHelper;
+	private EditText isbnInput;
 	
 	//Same index used to map from database table column to layout ID in item view
 //	private String[] mapFrom = WishlistDbAdapter.allColumns;
@@ -22,6 +30,18 @@ public class Wishlist extends ListActivity {
         setContentView(R.layout.main);
         mDbHelper = new WishlistDbAdapter(this);
         mDbHelper.open();
+        //initialize text input listener
+        isbnInput = (EditText)findViewById(R.id.input_isbn); 
+        isbnInput.setOnKeyListener(new OnKeyListener(){
+			@Override
+			public boolean onKey(View v, int keyCode, KeyEvent event) {
+				if((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)){
+					createItem(isbnInput.getText().toString());
+					return true;
+				}
+				return false;
+			}
+        });
         fillData();
     }
 
@@ -32,5 +52,31 @@ public class Wishlist extends ListActivity {
     	
     	SimpleCursorAdapter items = new SimpleCursorAdapter(this, R.layout.list_item, c, mapFrom, mapTo);
     	setListAdapter(items);
+    }
+    
+    private void createItem(String isbn){
+    	Toast.makeText(this, isbn, Toast.LENGTH_SHORT).show();
+		isbnInput.setVisibility(View.GONE);
+		isbnInput.setText("");
+    }
+    /*Menu related functions*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+    	getMenuInflater().inflate(R.menu.main, menu);
+    	return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()){
+    	case R.id.add_text:
+    		isbnInput.setVisibility(View.VISIBLE);
+    		isbnInput.requestFocus();
+    		return true;
+    	case R.id.add_barcode:
+    		//Call external scanner via Intent
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
     }
 }
