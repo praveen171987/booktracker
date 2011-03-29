0b2d304aa455d0d0d8749f0b1652d0bc3c9f4a1c
levan.matthew <levan.matthew@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-03-29 05:02:36 +0000

    context menu, layout w/ pictures.  Next: edit screen, more context options

--- Wishlist/gen/com/softwaresmithy/R.java
+++ Wishlist/gen/com/softwaresmithy/R.java
@@ -12,24 +12,30 @@ public final class R {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
+        public static final int unknown=0x7f020001;
     }
     public static final class id {
-        public static final int add_barcode=0x7f060003;
-        public static final int add_text=0x7f060002;
-        public static final int input_isbn=0x7f060001;
-        public static final int text1=0x7f060000;
+        public static final int add_barcode=0x7f060006;
+        public static final int add_text=0x7f060005;
+        public static final int author=0x7f060001;
+        public static final int cover=0x7f060000;
+        public static final int edit_item=0x7f060004;
+        public static final int input_isbn=0x7f060003;
+        public static final int title=0x7f060002;
     }
     public static final class layout {
         public static final int list_item=0x7f030000;
         public static final int main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f050000;
+        public static final int item_menu=0x7f050000;
+        public static final int main=0x7f050001;
     }
     public static final class string {
         public static final int add_barcode=0x7f040003;
         public static final int add_text=0x7f040002;
         public static final int app_name=0x7f040001;
+        public static final int edit_item=0x7f040004;
         public static final int hello=0x7f040000;
     }
 }
--- Wishlist/src/com/softwaresmithy/Wishlist.java
+++ Wishlist/src/com/softwaresmithy/Wishlist.java
@@ -1,8 +1,33 @@
 package com.softwaresmithy;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import android.app.ListActivity;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -17,17 +42,20 @@ public class Wishlist extends ListActivity {
 	private int mNoteNumber = 1;
 	private WishlistDbAdapter mDbHelper;
 	private EditText isbnInput;
+	private SimpleCursorAdapter listData;
 	
 	//Same index used to map from database table column to layout ID in item view
 //	private String[] mapFrom = WishlistDbAdapter.allColumns;
-	private String[] mapFrom = new String[]{WishlistDbAdapter.allColumns[3]};	
-	private int[] mapTo = new int[]{R.id.text1};
+	private String[] mapFrom = new String[]{WishlistDbAdapter.allColumns[3], WishlistDbAdapter.allColumns[4]};	
+	private int[] mapTo = new int[]{R.id.title, R.id.author};
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        registerForContextMenu(getListView());
+        
         mDbHelper = new WishlistDbAdapter(this);
         mDbHelper.open();
         //initialize text input listener
@@ -44,26 +72,100 @@ public class Wishlist extends ListActivity {
         });
         fillData();
     }
-
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo){
+    	super.onCreateContextMenu(menu, v, menuInfo);
+    	getMenuInflater().inflate(R.menu.item_menu, menu);
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+    	Toast.makeText(this, "I've been clicked!", Toast.LENGTH_SHORT).show();
+    	return super.onContextItemSelected(item);
+    }
+    
     private void fillData(){
     	//Get all rows from DB
     	Cursor c = mDbHelper.getAll();
     	startManagingCursor(c);
     	
-    	SimpleCursorAdapter items = new SimpleCursorAdapter(this, R.layout.list_item, c, mapFrom, mapTo);
-    	setListAdapter(items);
+    	listData = new ImageCursorAdapter(this, R.layout.list_item, c, mapFrom, mapTo,getExternalCacheDir().getAbsolutePath());
+    	setListAdapter(listData);
     }
     
     private void createItem(String isbn){
     	if(validIsbn(isbn)){
 			isbnInput.setVisibility(View.GONE);
 			isbnInput.setText("");
+			// 1) get basic info from xISBN
+			addItemToDB(isbn);
+			getImages(isbn);
+			listData.notifyDataSetChanged();
     	}else{
     		Toast.makeText(this, isbn+" is not a valid ISBN", Toast.LENGTH_SHORT).show();
     	}
     }
     
-    public boolean validIsbn(String isbn){
+    private void getImages(String isbn) {
+		HttpClient client = new DefaultHttpClient();
+		//http://imagesa.btol.com/ContentCafe/Jacket.aspx?UserID=ContentCafeClient&Password=Client&Type=S&Value=9780857660763
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("UserID", "ContentCafeClient"));
+		params.add(new BasicNameValuePair("Password", "Client"));
+		params.add(new BasicNameValuePair("Type", "S"));
+		params.add(new BasicNameValuePair("Value", isbn));
+		try {
+			URI btol = URIUtils.createURI("http", "imagesa.btol.com", -1, "/contentCafe/Jacket.aspx", URLEncodedUtils.format(params, "UTF-8"), null);
+			HttpGet cover = new HttpGet(btol);
+			HttpResponse resp = client.execute(cover);
+			if(resp.getEntity().getContentType().getValue().equals("image/jpeg")){
+				Bitmap image = BitmapFactory.decodeStream(resp.getEntity().getContent());
+				File file = new File(getExternalCacheDir(),isbn+".jpg");
+				image.compress(CompressFormat.JPEG, 95, new FileOutputStream(file));
+			}
+		}catch(Exception e){
+			Log.e(this.getClass().getName(), e.getMessage(), e);
+		}
+		
+	}
+
+	private void addItemToDB(String isbn) {
+		HttpClient client = new DefaultHttpClient();
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("method", "getMetadata"));
+		params.add(new BasicNameValuePair("format", "json"));
+		params.add(new BasicNameValuePair("ai", "scumbkt19"));
+		params.add(new BasicNameValuePair("fl", "title,author"));
+		try {
+			URI uri  = URIUtils.createURI("http", "xisbn.worldcat.org", -1, "/webservices/xid/isbn/"+isbn, URLEncodedUtils.format(params, "UTF-8"), null);
+			HttpGet xIsbn = new HttpGet(uri);
+			HttpResponse resp = client.execute(xIsbn);
+			String jsonResp = EntityUtils.toString(resp.getEntity());
+			Log.d(this.getClass().getName(), "json: "+jsonResp);
+			JSONObject obj = (JSONObject) new JSONTokener(jsonResp).nextValue();
+			//Parse object
+			if(!obj.getString("stat").equals("ok")){
+				Toast.makeText(this, "an error occured: "+obj.getString("stat"), Toast.LENGTH_SHORT).show();
+			}else {
+				JSONArray list = obj.getJSONArray("list");
+				JSONObject item = (JSONObject) list.get(0);
+				String title = null, author = null;
+				if(item.has("title")){
+					title = item.getString("title");
+				}
+				if(item.has("author")){
+					author = item.getString("author");
+				}
+				mDbHelper.createItem(isbn, null, title, author, null);
+			}
+			
+		} catch (Exception e) {
+			Log.e(this.getClass().getName(), e.getMessage(), e);
+		} 
+	}
+
+	public boolean validIsbn(String isbn){
     	char[] nums = isbn.toCharArray();
     	if(nums.length == 10){
     		if(nums[9]=='X' || nums[9]=='x'){
