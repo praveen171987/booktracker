b0f5b074922e7a90e3d07d526595f787989b5a99
levan.matthew <levan.matthew@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-05-12 20:15:21 +0000

    Added a Preferences activity accessible from the application menu.

---
+++ Wishlist/src/com/softwaresmithy/Preferences.java
@@ -0,0 +1,14 @@
+package com.softwaresmithy;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.global_prefs);
+	}
+	
+}
---
+++ Wishlist/src/com/softwaresmithy/library/impl/Default.java
@@ -0,0 +1,12 @@
+package com.softwaresmithy.library.impl;
+
+import com.softwaresmithy.library.Library;
+
+public class Default extends Library {
+
+	@Override
+	public void init(String... strings) {
+
+	}
+
+}
