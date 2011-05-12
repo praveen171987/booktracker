7150a4618fe4d72dcdcb5e266fe0bb9f012df8f3
levan.matthew <levan.matthew@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-05-12 04:29:53 +0000

    OMG, Factories! Implementations! Interfaces! this is starting to look like a real project!

---
+++ Wishlist/src/com/softwaresmithy/library/impl/WebPac.java
@@ -0,0 +1,63 @@
+package com.softwaresmithy.library.impl;
+
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import android.util.Log;
+
+import com.softwaresmithy.library.LibStatus;
+import com.softwaresmithy.library.Library;
+
+public class WebPac extends Library implements LibStatus{
+	private String isbnSearchUrl;
+	@Override
+	public void init(String... strings) {
+		if(strings.length > 0){
+			isbnSearchUrl = strings[0];
+		}
+		
+	}
+	@Override
+	public STATUS checkAvailability(String isbn) {
+    	HttpGet get = null;
+    	try{
+        	HttpClient client = new DefaultHttpClient();
+			get = new HttpGet(String.format(isbnSearchUrl, isbn));
+			HttpResponse resp = client.execute(get);
+			Scanner s = new Scanner(resp.getEntity().getContent());
+			String pattern = s.findWithinHorizon("((\\d*) hold[s]? on first copy returned of (\\d*) )?[cC]opies", 0);
+			
+			if(pattern != null){
+				MatchResult match = s.match();
+				if(match.groupCount() == 3){
+					if(match.group(2) == null){
+						return STATUS.AVAILABLE;
+					}else{
+						int numHolds = Integer.parseInt(match.group(2));
+						int numCopies = Integer.parseInt(match.group(3));
+						if(numHolds < numCopies){
+							return STATUS.SHORT_WAIT;
+						}else if(numHolds >= numCopies && numHolds <= (2*numCopies)){
+							return STATUS.WAIT;
+						}else{
+							return STATUS.LONG_WAIT;
+						}
+					}
+				}
+			}
+			return STATUS.NO_MATCH;
+    	}catch(Exception e){
+			Log.e(this.getClass().getName(), e.getMessage(), e);
+			return null;       		
+    	}finally{
+    		if(get != null)
+    			get.abort();
+    	}
+	}
+
+}
