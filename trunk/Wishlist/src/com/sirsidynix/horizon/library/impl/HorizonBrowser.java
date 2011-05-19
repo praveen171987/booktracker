fe72a175af6cf697893ebaecfa84e06d053290a0
jesse.d.hess@gmail.com <jesse.d.hess@gmail.com@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-05-19 00:32:03 +0000

    Updated HorizonBrowser with re-factor and added test class to the test project, also moved my data files to the test project

--- trunk/Wishlist/src/com/sirsidynix/horizon/library/impl/HorizonBrowser.java
+++ trunk/Wishlist/src/com/sirsidynix/horizon/library/impl/HorizonBrowser.java
@@ -1,201 +1,57 @@
 package com.sirsidynix.horizon.library.impl;
 
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpression;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.protocol.HTTP;
-import org.apache.http.util.EntityUtils;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
+import com.sirsidynix.horizon.library.HorizonStatus;
+import com.sirsidynix.horizon.library.HorizonTools;
 import com.softwaresmithy.library.LibStatus;
 import com.softwaresmithy.library.Library;
 
+/**
+ * The Horizon WishList Library and LibStatus implementation
+ * @author Jesse Hess
+ *
+ */
 public class HorizonBrowser extends Library implements LibStatus {
 	
 	private String url;
-	
+	private String isbnIndex;
+
+	/**
+	 * init uses the first parameter as the URL and the second parameter as the index for the key needed in an ISBN search
+	 */
 	@Override
 	public void init(String... strings) {
 		if(strings.length > 0){
-			url = strings[0];
+			this.url = strings[0];
+		}
+		if (strings.length > 1) {
+		  this.isbnIndex = strings[1];
 		}
 	}
 	
+	/**
+	 * returns the availability of an ISBN from a Horizon ISBN search
+	 */
 	@Override
 	public STATUS checkAvailability(String isbn) {
-		String xmlGet = httpGet(url, "auth=true", "GetXML=true");
-		Element element = getElement(xmlGet);
-		String sessionId = getStringFromXPath(element, "/searchresponse/session | /patronpersonalresponse/session");
-
-		List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-		parameters.add(new BasicNameValuePair("session", sessionId));
-		parameters.add(new BasicNameValuePair("index", "PISBNEX"));
-		parameters.add(new BasicNameValuePair("term", isbn));
-		parameters.add(new BasicNameValuePair("ipp", "150"));
-		parameters.add(new BasicNameValuePair("GetXML", "true"));
-				
-		String xmlPost = httpPost(url, parameters);
-		element = getElement(xmlPost);
-		
-		NodeList nodeList = getNodeListFromXPath(element,"/searchresponse/items/searchresults/results/row");
-		String requestsColumn = getStringFromXPath(element, "count(/searchresponse/fullnonmarc/searchresults/header/col[label='Requests']/preceding-sibling::*)+1");
-		STATUS waitStatus = STATUS.NO_MATCH;
-		try {
-			int columnNum = Integer.parseInt(requestsColumn);
-			String requestsStr = getStringFromXPath(element, "/searchresponse/fullnonmarc/searchresults/results/row/cell["+columnNum+"]/data/text");
-			int requests = Integer.parseInt(requestsStr);
-			if (nodeList != null) {
-				int copies = nodeList.getLength();
-				if (requests <= 0 && copies > 0) {
-					waitStatus = STATUS.AVAILABLE;
-				} else if (requests > 0 && requests < copies) {
-					waitStatus = STATUS.SHORT_WAIT;
-				} else if (requests > 0 && copies <= requests && requests <= 2*copies) {
-					waitStatus = STATUS.WAIT;
-				} else if (requests >0 && requests > 2*copies) {
-					waitStatus = STATUS.LONG_WAIT;
-				}
-			}
-		} catch (NumberFormatException e) {
-			// ignore
-		}
-		NodeList errorList = getNodeListFromXPath(element, "//error");
-		for (int i = 0; i < errorList.getLength(); i++) {
-			Node error = errorList.item(i);
-			String errorType = getStringFromXPath(error,"type");
-			String errorSubject = getStringFromXPath(error,"subject");
-			String errorReason = getStringFromXPath(error,"reason");
-			String errorMessage = getStringFromXPath(error,"message");			
-
-			if (!isEmpty(errorType + errorSubject + errorReason + errorMessage)) {
-				waitStatus = STATUS.NO_MATCH;
-			}
-		}
-		return waitStatus;
-	}
-	
-	private String httpGet(String url, String... parameters) {
-		String response = "";
-		StringBuilder b = new StringBuilder();
-		boolean isFirst = true;
-		for (String parameter : parameters) {
-			b.append((isFirst ? "?" : "&") + parameter);
-			isFirst = false;
-		}
-		try {
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpGet httpget = new HttpGet(url + b.toString());
-			HttpResponse httpGetResponse = httpClient.execute(httpget);
-			response = EntityUtils.toString(httpGetResponse.getEntity());
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
-
-	private String httpPost(String url, List<NameValuePair> parameters) {
-		String response = "";
-		try {
-			StringBuilder b = new StringBuilder();
-			boolean isFirst = true;
-			for (NameValuePair parameter : parameters) {
-				b.append((isFirst ? "?" : "&") + parameter.getName() + "=" + parameter.getValue());
-				isFirst = false;
-			}
-			UrlEncodedFormEntity form = new UrlEncodedFormEntity(parameters, HTTP.UTF_8);
-			HttpPost httpPost = new HttpPost(url);
-			httpPost.setEntity(form);
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpResponse postResponse = httpClient.execute(httpPost);
-			response = EntityUtils.toString(postResponse.getEntity());
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return response;
-	}
-	
-	private Element getElement(String xmlString) {
-		Element element = null;
-		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-		try {
-			DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-			InputSource inputSource = new InputSource();
-			inputSource.setCharacterStream(new StringReader(xmlString));
-			Document document = documentBuilder.parse(inputSource);
-			element = document.getDocumentElement();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return element;
-	}
-
-	private String getStringFromXPath(Node node, String expression) {
-		String nodeResult = "";
-		try {
-			XPathFactory factory = XPathFactory.newInstance();
-			XPath xpath = factory.newXPath();
-			XPathExpression expr = xpath.compile(expression);
-			nodeResult = (String) expr.evaluate(node, XPathConstants.STRING);
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();
-		}
-		return nodeResult;
-	}
-	
-	private NodeList getNodeListFromXPath(Node node, String expression) {
-		NodeList nodeResult = null;
-		try {
-			XPathFactory factory = XPathFactory.newInstance();
-			XPath xpath = factory.newXPath();
-			XPathExpression expr = xpath.compile(expression);
-			nodeResult = (NodeList) expr.evaluate(node, XPathConstants.NODESET);
-		} catch (XPathExpressionException e) {
-			e.printStackTrace();
-		}
-		return nodeResult;
-	}
-
-	private boolean isEmpty(String s) {
-		boolean value = false;
-		if (s == null || "".equals(s)) {
-			value = true;
-		}
-		return value;
+	  HorizonTools tools = new HorizonTools();
+	  HorizonStatus horizonStatus = tools.searchIsbnForStatus(this.url, this.isbnIndex, isbn);
+	  
+	  STATUS wishListStatus = STATUS.NO_MATCH;
+	  if (HorizonStatus.AVAILABLE == horizonStatus) {
+	    wishListStatus = STATUS.AVAILABLE;
+	  } else if (HorizonStatus.SHORT_WAIT == horizonStatus) {
+	    wishListStatus = STATUS.SHORT_WAIT;
+    } else if (HorizonStatus.WAIT == horizonStatus) {
+      wishListStatus = STATUS.WAIT;
+    } else if (HorizonStatus.LONG_WAIT == horizonStatus) {
+      wishListStatus = STATUS.LONG_WAIT;
+    } else if (HorizonStatus.NO_COPIES == horizonStatus) {
+      wishListStatus = STATUS.NO_MATCH;
+    } else if (HorizonStatus.NO_MATCH == horizonStatus) {
+      wishListStatus = STATUS.NO_MATCH;
+    } else if (HorizonStatus.ERROR == horizonStatus) {
+      wishListStatus = STATUS.NO_MATCH;
+    }
+		return wishListStatus;
 	}
 }
---
+++ trunk/WishlistTest/src/com/sirsidynix/horizon/library/test/HorizonToolsTest.java
@@ -0,0 +1,125 @@
+package com.sirsidynix.horizon.library.test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.sirsidynix.horizon.library.HorizonStatus;
+import com.sirsidynix.horizon.library.HorizonTools;
+
+/**
+ * Test the HorizonTools class
+ * @author Jesse Hess
+ * TODO: flesh this out, figure out how to simulate responses using local XML
+ */
+public class HorizonToolsTest {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // for testing ISBN
+//    String isbn = "9780765315151"; // 1 result
+//    String isbn = "9780441020379"; // no results
+//    String isbn = "9781436178518"; // 2 results
+//    String isbn = "9780312651206"; // held item
+//    String isbn = "9780394900018"; // cat in the hat
+//    String isbn = "9780394800011"; // cat in the hat
+//    String isbn = "0340364777"; // IT
+//    String isbn = "0450411435"; // IT
+//    String isbn = "9780743292337"; // Cell
+    String isbn = "0375504397"; // black house
+//    String isbn = "0670260770"; // the dead zone
+//    String isbn = "0451155750"; // the dead zone
+    Map<String, String> urlMap = new LinkedHashMap<String, String>();
+    urlMap.put("http://192.69.117.21/ipac20/ipac.jsp", ""/*"ISBNEX"*/); // amherst co, VA
+    urlMap.put("http://216.54.20.117/ipac20/ipac.jsp", ""/*"ISBNEX"*/); // portsmouth public, VA
+    urlMap.put("http://catalog.lcpl.lib.va.us/ipac20/ipac.jsp", "PISBNEX"); // loudoun co, VA
+    urlMap.put("http://catalog.wrl.org/ipac20/ipac.jsp", "ISBNEX"); // williamsburg regional, VA
+    urlMap.put("http://hip.hamptonpubliclibrary.org/ipac20/ipac.jsp", "ISBNEX"); // hampton public, VA
+    urlMap.put("http://hip.hcplonline.info/ipac20/ipac.jsp", "ISBNEX"); // harford co, MD
+    urlMap.put("http://horizon.samuelslibrary.net/ipac20/ipac.jsp", "ISBNEX"); // samuels public, VA
+    urlMap.put("http://ipac.librarypoint.org/ipac20/ipac.jsp", "ISBNE"); // central rappahannock regional, VA
+    urlMap.put("http://ipac.yorkcounty.gov:8080/ipac20/ipac.jsp", "ISBNEX"); // york co, VA
+    // other horizon libraries
+//    urlMap.put("http://146.74.92.18/ipac20/ipac.jsp", "ISBNEX"); // request positive
+//    urlMap.put("http://199.120.252.134/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://216.57.213.200/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://catalog.lcpl.lib.va.us/ipac20/ipac.jsp", "PISBNEX"); // available
+//    urlMap.put("http://catalog.mylibrary.us/ipac20/ipac.jsp", "ISBNEX"); // request positive
+//    urlMap.put("http://bronte.egvpl.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://64.60.64.101:81/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://4.22.114.43/ipac20/ipac.jsp", "ISBN"); // 0 matches, available
+//    urlMap.put("http://66.195.188.172/ipac20/ipac.jsp", "ISBNEX"); // no copies, available
+//    urlMap.put("http://catalog.hcpl.net/ipac20/ipac.jsp", "ISBNEXH"); // no copies, available
+//    urlMap.put("http://catalog.spl.org/ipac20/ipac.jsp", "ISBNZEX"); // no match, available
+//    urlMap.put("http://192.69.117.21/ipac20/ipac.jsp", "ISBNEX"); // legit no copies of dr suess
+//    urlMap.put("http://capemay.njstatelib.org/ipac20/ipac.jsp", "ISBNEX"); // 0 matches, available
+//    urlMap.put("http://catalog.vencolibrary.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://catalog.westportlibrary.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://comarms.ipac.dynixasp.com/ipac20/ipac.jsp", "ISBN"); // available
+//    urlMap.put("http://hip.hcplonline.info/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://hip.plcmc.org/ipac20/ipac.jsp", "ISBNEX"); // requests positive
+//    urlMap.put("http://hip1.sjvls.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://ipac.anaheim.net/ipac20/ipac.jsp", "ISBNE"); // available
+//    urlMap.put("http://ipac.hcplc.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://ipac.librarypoint.org/ipac20/ipac.jsp", "ISBNE"); // available
+//    urlMap.put("http://ipac2.vpl.ca/ipac20/ipac.jsp", "ISBNEX"); // short wait, available
+//    urlMap.put("http://lincpac.lincolntrail.info:8080/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://search.idaho-lynx.org/ipac20/ipac.jsp", "ISBNEX"); // available
+//    urlMap.put("http://webpac.barringtonarealibrary.org/ipac20/ipac.jsp", ".BN"); // available
+//    urlMap.put("http://www3.mississauga.ca/ipac20/ipac.jsp", "ISBN"); // available
+//    urlMap.put("https://libcat.uchicago.edu/ipac20/ipac.jsp", "ISBN"); // available
+    // still testing
+//    urlMap.put("http://134.241.121.88/ipac20/ipac.jsp", "ISBNEX"); // more than 1 copy problem for dr suess
+//    urlMap.put("http://ipac.kentonlibrary.org:8080/ipac20/ipac.jsp", "ISBNB"); // seems to only support ISBN "browse" search
+    // not quite working libraries
+//    urlMap.put("http://helix.usc.edu/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://hip.rpls.ws/ipac20/ipac.jsp", "ISBN"); // no copies
+//    urlMap.put("http://ipac.canterbury.ac.nz/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://ipac.ci.escondido.ca.us/ipac20/ipac.jsp", "ISBN"); // no copies
+//    urlMap.put("http://ipac.lewisham.gov.uk/ipac20/ipac.jsp", "ISBNEX"); // 0 matches
+//    urlMap.put("http://ipac.wrhs.org:8080/ipac20/ipac.jsp", "ISBNEX"); // 0 matches
+//    urlMap.put("http://leonardo.lindahall.org/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://libraries.rochdale.gov.uk/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://merlin.liu.edu/ipac20/ipac.jsp", "ISBN"); // no copies
+//    urlMap.put("http://ohip.hpl.ca/ipac20/ipac.jsp", "ISBN"); // no copies
+//    urlMap.put("http://ouipac.ouboces.org/ipac20/ipac.jsp", "ISBN"); // no copies
+//    urlMap.put("http://webcat.hud.ac.uk/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://webpac.twu.ca/ipac20/ipac.jsp", "ISBN");  // 0 matches
+//    urlMap.put("http://www.biblartepac.gulbenkian.pt/ipac20/ipac.jsp", "ISBN"); // 0 matches
+//    urlMap.put("http://www3.cca.qc.ca/ipac20/ipac.jsp", "ISBN"); // 0 matches
+    // failing
+//    urlMap.put("http://catalog.ccpa.net/ipac20/ipac.jsp", "PISBN"); // something wrong, definitely has 0451155750, but says no copies
+//    urlMap.put("http://catalog.mvlc.org/ipac20/ipac.jsp", "ISBNEX"); // no copies, possibly requires login to see holdings
+//    urlMap.put("http://catalog.pasadenapubliclibrary.net/ipac20/ipac.jsp", "ISBN"); // no copies, definitely has 0375504397, something wrong
+//    urlMap.put("http://catalog.tscpl.org/ipac20/ipac.jsp", "ISBN"); // no copies, definitely has 0375504397, but no results
+//    urlMap.put("http://libserv.aip.org:81/ipac20/ipac.jsp", ""); // does not seem to support ISBN search
+//    urlMap.put("http://216.68.116.14:8081/ipac20/ipac.jsp", ""); // does not seem to support ISBN search
+//    urlMap.put("http://ipac.librarieshawaii.org/ipac20/ipac.jsp", ""); // does not seem to support ISBN search
+//    urlMap.put("http://libserv.aip.org:81/ipac20/ipac.jsp", ""); // does not seem to support ISBN search
+//    urlMap.put("http://ipac.selco.info/ipac20/ipac.jsp"); // requires login
+//    urlMap.put("http://ipac.infolynx.org/ipac20/ipac.jsp"); // requires login
+//    urlMap.put("http://ipac.selco.info/ipac20/ipac.jsp"); // requires login   
+//    urlMap.put("http://wpl-pac.winnipeg.ca/ipac20/ipac.jsp", ""); // URL encoding a properly encoded submenu, the submenu that fails is the one with the ISBN
+//    urlMap.put("http://siris-archives.si.edu/ipac20/ipac.jsp", ""); // Expected left paren or shortcut, misplaced symbol: 0375504397
+//    urlMap.put("http://206.187.12.10/ipac20/ipac.jsp", ""); // Expected left paren or shortcut, misplaced symbol: 0375504397
+//    urlMap.put("http://216.170.15.165/ipac20/ipac.jsp", "");  // Expected left paren or shortcut, misplaced symbol: 0375504397
+
+    HorizonTools tools;
+    if (args.length > 1) {
+      String proxyUrl = args[0];
+      int proxyPort = Integer.parseInt(args[1]);
+      tools = new HorizonTools(proxyUrl, proxyPort);
+    } else {
+      tools = new HorizonTools();
+    }
+        
+    int count = 0;
+    for (String url : urlMap.keySet()) {
+      System.out.println((count+1)+" of "+urlMap.size());
+      HorizonStatus status = tools.searchIsbnForStatus(url, urlMap.get(url), isbn);
+      System.out.println(status+" for "+ url);
+      count++;
+    }
+  }
+}
