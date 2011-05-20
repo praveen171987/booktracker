4fd3da193904e71c1dd0d439b5605dd5eca52e95
jesse.d.hess@gmail.com <jesse.d.hess@gmail.com@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-05-20 19:54:10 +0000

    Re-factor to LibraryLib project

--- Wishlist/src/com/softwaresmithy/library/Library.java
+++
@@ -1,8 +0,0 @@
-package com.softwaresmithy.library;
-
-import java.net.URISyntaxException;
-
-public abstract class Library {
-	public abstract void init(String...strings);
-	public abstract boolean isCompatible(String url) throws URISyntaxException;
-}
--- Wishlist/src/com/tlcdelivers/librarysolution/library/LibrarySolutionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.tlcdelivers.librarysolution.library;
-
-public enum LibrarySolutionStatus {
-	AVAILABLE, 
-	NO_MATCH, 
-	WAIT
-}
