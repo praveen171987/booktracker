d1e7a4a71d002d87abdf19a8a5eedbc62bcf9bc1
levan.matthew <levan.matthew@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-08-07 05:05:07 +0000

    Fleshed out Library class, added a Util class for multi valued maps

---
+++ LibraryLib/src/com/softwaresmithy/util/MapList.java
@@ -0,0 +1,31 @@
+package com.softwaresmithy.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapList<K, E> {
+	private Map<K, List<E>> map = new HashMap<K, List<E>>();
+	
+	public void addList(K key, List<E> newItems) {
+		if(map.containsKey(key)){
+			map.get(key).addAll(new ArrayList<E>(newItems));
+		}else {
+			map.put(key, newItems);
+		}
+	}
+	
+	public void addItem(K key, E newItem) {
+		if(map.containsKey(key)){
+			map.get(key).add(newItem);
+		}else {
+			map.put(key, new ArrayList<E>());
+			map.get(key).add(newItem);
+		}
+	}
+	
+	public List<E> getList(K key) {
+		return map.get(key);
+	}
+}
