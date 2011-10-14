62c295ba4d362a239aa8a1cd4a4d83ca39e569f1
levan.matthew@gmail.com <levan.matthew@gmail.com@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-10-14 03:38:34 +0000

    Long pressing a book will give you the option to ping its status, this is a baby step towards getting a service that will do this automatically and regularly.  When the status has changed to AVAILABLE, a Notification is generated, otherwise a Toast is shown to the user. Added a STATUS spinner to the editItem screen, mostly for debugging but I might keep it.

--- Wishlist/src/com/softwaresmithy/library/impl/Default.java
+++ Wishlist/src/com/softwaresmithy/library/impl/Default.java
@@ -1,17 +1,20 @@
 package com.softwaresmithy.library.impl;
 
+import java.util.Map;
+
 import com.softwaresmithy.library.Library;
 
 public class Default extends Library {
 
 	@Override
-	public void init(String... strings) {
-
+	public boolean isCompatible(String url) {
+		return true;
 	}
 
 	@Override
-	public boolean isCompatible(String url) {
-		return true;
+	public void init(Map<String, String> args) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
