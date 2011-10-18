d6a60d9c0db1727a601f1e12b7a579c9423e3fae
levan.matthew@gmail.com <levan.matthew@gmail.com@c5e6f7e9-6356-0410-84d0-c5af5997a13e>
2011-10-18 03:26:33 +0000

    Work on import/export functionality

---
+++ trunk/Wishlist/src/com/softwaresmithy/DataImportExport.java
@@ -0,0 +1,36 @@
+package com.softwaresmithy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class DataImportExport extends Activity {
+
+	private static final int PICK_FILE = 0;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.import_export);
+	}
+
+	public void invokePickFile(View button) {
+		Intent intent = new Intent("org.openintents.action.PICK_FILE");
+		intent.setData(Uri.parse("file:///sdcard/notepad.csv"));
+		intent.putExtra("org.openintents.extra.TITLE", "Please select a file");
+		startActivityForResult(intent, PICK_FILE);
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(PICK_FILE == requestCode && resultCode == RESULT_OK){
+			Uri uri = data.getData();
+			((EditText)findViewById(R.id.import_file_text)).setText(uri.toString());
+		}
+	}
+}
---
+++ trunk/Wishlist/src/com/softwaresmithy/view/CategoryDivider.java
@@ -0,0 +1,30 @@
+package com.softwaresmithy.view;
+
+import android.R;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * I'd like to do this by making a style that extends listSeparatorTextViewStyle, but unfortunately, 
+ * it's private :(
+ * @author matt
+ *
+ */
+public class CategoryDivider extends TextView {
+
+	public CategoryDivider(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		int vertPadding = (int) Math.round(2.0*getResources().getDisplayMetrics().density);
+		int leftPadding = (int) Math.round(5.0*getResources().getDisplayMetrics().density);
+		setPadding(leftPadding, vertPadding, vertPadding, 0);
+	}
+
+	public CategoryDivider(Context context, AttributeSet attrs) {
+		this(context, attrs, R.attr.listSeparatorTextViewStyle);
+	}
+	
+	public CategoryDivider(Context context) {
+		this(context, null, R.attr.listSeparatorTextViewStyle);
+	}
+}
